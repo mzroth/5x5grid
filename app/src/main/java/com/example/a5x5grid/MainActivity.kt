c3710fabf9fce4cf0567bc182eadc4959340fa3c
Mark Roth <mzroth@gmail.com>
2019-03-14 21:47:27 -0400

    :)

--- app/src/main/java/com/example/a5x5grid/MainActivity.kt
+++ app/src/main/java/com/example/a5x5grid/MainActivity.kt
@@ -11,10 +11,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    fun changeColor(view: View) {
-        
-    }
-
-
 }
