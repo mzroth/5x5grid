96c9a47040828b4963770b7ce7ac835ccbf3961f
Mark Roth <mzroth@gmail.com>
2019-03-15 13:16:48 -0400

    fixed comments regarding inline

--- app/src/main/java/com/example/a5x5grid/MainActivity.kt
+++ app/src/main/java/com/example/a5x5grid/MainActivity.kt
@@ -20,11 +20,11 @@ class MainActivity : AppCompatActivity() {
             button5_1, button5_2, button5_3, button5_4, button5_5
         ).forEach {
             it.setOnClickListener {
-                val color = (it.background as? ColorDrawable)?.color
-                when(color) {
-                    ContextCompat.getColor(this, R.color.button_default) -> it.setBackgroundColor(ContextCompat.getColor(this, R.color.button_pressed))
-                    ContextCompat.getColor(this, R.color.button_pressed) -> it.setBackgroundColor(ContextCompat.getColor(this, R.color.button_default))
-                }
+                it.setBackgroundColor(ContextCompat.getColor(this, when((it.background as? ColorDrawable)?.color) {
+                    ContextCompat.getColor(this, R.color.button_default) -> R.color.button_pressed
+                    ContextCompat.getColor(this, R.color.button_pressed) -> R.color.button_default
+                    else -> 0
+                }))
             }
         }
     }
